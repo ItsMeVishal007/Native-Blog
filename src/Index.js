65d7259373ec70886ace6569ee5795047fbf672a
Cd-Vishal007 <vishal.codedrill@gmail.com>
2021-08-13 15:55:13 +0530

    initializing the app

---
+++ src/Index.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { Text, View, StyleSheet } from "react-native";
+
+const Index = () => {
+  return (
+    <View>
+      <Text>This is Index Page</Text>
+    </View>
+  );
+};
+
+export default Index;
