57aac022717bb2164127220b90c30b6b87868973
Cd-Vishal007 <vishal.codedrill@gmail.com>
2021-08-14 17:42:29 +0530

    del the blog by created a new case with payload communication

--- src/contexts/BlogContext.js
+++ src/contexts/BlogContext.js
@@ -8,10 +8,12 @@ const reducer = (state, action) => {
       return [
         ...state,
         {
-          id: Math.floor(Math.random() * 9999),
+          id: Math.floor(Math.random() * 99999),
           title: `This is blog ${state.length + 1}`,
         },
       ];
+    case "deleteBlog":
+      return state.filter((blogpost) => blogpost.id != action.payload);
     default:
       throw new Error();
   }
@@ -24,8 +26,12 @@ export const BlogProvider = ({ children }) => {
     dispatch({ type: "addBlog" });
   };
 
+  const delBlog = (id) => {
+    dispatch({ type: "deleteBlog", payload: id });
+  };
+
   return (
-    <BlogContext.Provider value={{ blogPost: state, addBlog }}>
+    <BlogContext.Provider value={{ blogPost: state, addBlog, delBlog }}>
       {children}
     </BlogContext.Provider>
   );
